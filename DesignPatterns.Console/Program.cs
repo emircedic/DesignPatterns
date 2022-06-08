236bf61c7fa52a368ee076c4141f9746b4f59bd8
Emir Ćedić <45953377+emircedic@users.noreply.github.com>
2022-06-08 22:02:25 +0200

    Aded design pattern "Prototype".

--- DesignPatterns.Console/Program.cs
+++ DesignPatterns.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DesignPatterns.AbstractFactory;
 using DesignPatterns.Builder;
 using DesignPatterns.FactoryMethod;
+using DesignPatterns.Prototype;
 
 #region Factory Method
 
@@ -28,29 +29,62 @@ using DesignPatterns.FactoryMethod;
 
 #region Builder
 
-Console.WriteLine("App: Launched with Builder  ");
+//Console.WriteLine("App: Launched with Builder  ");
 
-var director = new Director();
-var builder = new ConcreteBuilder();
+//var director = new Director();
+//var builder = new ConcreteBuilder();
 
-director.Builder = builder;
-director.BuildFullFeaturedProduct();
-var resultFullProduct = builder.GetProduct();
+//director.Builder = builder;
+//director.BuildFullFeaturedProduct();
+//var resultFullProduct = builder.GetProduct();
 
-director.BuildMinimalViableProduct();
-var resultMinimalProduct = builder.GetProduct();
+//director.BuildMinimalViableProduct();
+//var resultMinimalProduct = builder.GetProduct();
 
-Console.WriteLine(resultFullProduct.ListParts());
-Console.WriteLine(resultMinimalProduct.ListParts());
+//Console.WriteLine(resultFullProduct.ListParts());
+//Console.WriteLine(resultMinimalProduct.ListParts());
 
-builder.BuildPartA();
-builder.BuildPartB();
+//builder.BuildPartA();
+//builder.BuildPartB();
 
-Console.WriteLine(builder.GetProduct().ListParts());
+//Console.WriteLine(builder.GetProduct().ListParts());
 
-var chainedBuilder = new ConcreteChainedBuilder();
+//var chainedBuilder = new ConcreteChainedBuilder();
 
-var product = chainedBuilder.BuildPartA().BuildPartC().BuildPartC().GetProduct();
-Console.WriteLine(product.ListParts());
+//var product = chainedBuilder.BuildPartA().BuildPartC().BuildPartC().GetProduct();
+//Console.WriteLine(product.ListParts());
+
+#endregion
+
+#region Prototype
+
+//Person p1 = new Person();
+//p1.Age = 42;
+//p1.BirthDate = Convert.ToDateTime("1977-01-01");
+//p1.Name = "Jack Daniels";
+//p1.IdInfo = new IdInfo(666);
+
+//Person p2 = p1.ShallowCopy();
+//Person p3 = p2.DeepCopy();
+
+//DisplayValues(p1);
+//DisplayValues(p2);
+//DisplayValues(p3);
+
+//p1.Age = 32;
+//p1.BirthDate = Convert.ToDateTime("1900-01-01");
+//p1.Name = "Frank";
+//p1.IdInfo.IdNumber = 7878;
+
+//Console.WriteLine("Changed values:");
+
+//DisplayValues(p1);
+//DisplayValues(p2);
+//DisplayValues(p3);
+
+//static void DisplayValues(Person p)
+//{
+//    Console.WriteLine(String.Format("Name: {0}, Age: {1}, BirthDate: {2}\n ID: {3}", p.Name, p.Age, p.BirthDate, p.IdInfo.IdNumber));
+//}
 
 #endregion
---
+++ DesignPatterns/Prototype/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPatterns.Prototype
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }
+        public IdInfo IdInfo { get; set; }
+        
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.Name = String.Copy(Name);
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+
+            return clone;
+        }
+    }
+
+    public class IdInfo
+    {
+        public int IdNumber { get; set; }
+
+        public IdInfo(int idNumber)
+        {
+            IdNumber = idNumber;
+        }
+    }
+}
