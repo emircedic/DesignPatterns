d2d1f681fda2d94a483f0a5f6754a5e66680fb1d
Emir Ćedić <45953377+emircedic@users.noreply.github.com>
2022-06-12 22:19:53 +0200

    Added new design pattern "Composite".

--- DesignPatterns.Console/Program.cs
+++ DesignPatterns.Console/Program.cs
@@ -6,6 +6,7 @@ using DesignPatterns.Singleton.SingletonNonThreadSafe;
 using DesignPatterns.Singleton.ThreadSafe;
 using DesignPatterns.Structural.Adapter;
 using DesignPatterns.Structural.Bridge;
+using DesignPatterns.Structural.Composite;
 
 #region Factory Method
 
@@ -145,13 +146,38 @@ using DesignPatterns.Structural.Bridge;
 
 #region Bridge
 
-Client client = new Client();
-Abstraction abstraction = new Abstraction(new ConcreteImplementationA());
-client.ClientCode(abstraction);
+//Client client = new Client();
+//Abstraction abstraction = new Abstraction(new ConcreteImplementationA());
+//client.ClientCode(abstraction);
 
-Console.WriteLine();
+//Console.WriteLine();
 
-abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
-client.ClientCode(abstraction);
+//abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+//client.ClientCode(abstraction);
+
+#endregion
+
+#region Composite 
+
+CompositeClient client = new CompositeClient();
+
+Leaf leaf = new Leaf();
+Console.WriteLine("Client: I get a simple component:");
+client.ClientCode(leaf);
+
+Composite tree = new Composite();
+Composite branch1 = new Composite();
+branch1.Add(new Leaf());
+branch1.Add(new Leaf());
+Composite branch2 = new Composite();
+branch2.Add(new Leaf());
+tree.Add(branch1);
+tree.Add(branch2);
+
+Console.WriteLine("Client: Now Ive got a composite tree:");
+client.ClientCode(tree);
+
+Console.WriteLine("Client: I dont need ot check the components classes even when managing the tree:\n");
+client.ClientCode2(tree, leaf);
 
 #endregion
---
+++ DesignPatterns/Structural/Composite/Component.cs
@@ -0,0 +1,87 @@
+﻿namespace DesignPatterns.Structural.Composite
+{
+    public abstract class Component
+    {
+        public Component() { }
+
+        public abstract string Operation();
+
+        public virtual void Add(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual bool IsComposite()
+        {
+            return true;
+        }
+    }
+
+    public class Leaf : Component
+    {
+        public override string Operation()
+        {
+            return "Leaf";
+        }
+        public override bool IsComposite()
+        {
+            return false;
+        }
+    }
+
+    public class Composite: Component
+    {
+        protected List<Component> _children = new List<Component>();
+
+        public override void Add(Component component)
+        {
+            _children.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            _children.Remove(component);
+        }
+
+        public override string Operation()
+        {
+            int i = 0;
+            string result = "Branch(";
+
+            foreach (Component component in _children)
+            {
+                result += component.Operation();
+                if (i != this._children.Count - 1)
+                    result += "+";
+
+                i++;
+            }
+
+            return result + ")";
+        }
+    }
+
+    public class CompositeClient
+    {
+        public void ClientCode(Component leaf)
+        {
+            Console.WriteLine($"RESULT: {leaf.Operation()}\n");
+        }
+
+        public void ClientCode2(Component component1, Component component2)
+        {
+            if (component1.IsComposite())
+            {
+                component1.Add(component2);
+            }
+
+            Console.WriteLine($"RESULT: {component1.Operation()}\n");
+        }
+    }
+
+}
